8b6707b3c78be831bef7fd16ef447ff405b959c7
Alexander Suvorov <suvorov.alexander15@gmail.com>
2024-09-16 21:37:07 +0300

    some intergration tests refactor

--- internal/db/repository/sources_test.go
+++ internal/db/repository/sources_test.go
@@ -4,15 +4,10 @@ import (
 	"context"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
-	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
-	"log"
+	"music-digest-bot/internal/testhelpers"
 	"testing"
-	"time"
 )
 
 var yaMusic = "yandexmusic"
@@ -20,29 +15,11 @@ var yaMusic = "yandexmusic"
 func TestSourcesRepository(t *testing.T) {
 	ctx := context.Background()
 
-	dbName := "music_digest_bot"
-	dbUser := "postgres"
-	dbPassword := "postgres"
-
-	postgresContainer, err := postgres.Run(ctx,
-		"docker.io/postgres:16-alpine",
-		postgres.WithDatabase(dbName),
-		postgres.WithUsername(dbUser),
-		postgres.WithPassword(dbPassword),
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(5*time.Second)),
-	)
+	pgContainer, err := testhelpers.CreatePostgresContainer(ctx)
 	require.NoError(t, err)
-	defer func() {
-		if err := postgresContainer.Terminate(ctx); err != nil {
-			log.Printf("failed to terminate container: %s", err)
-		}
-	}()
 
-	dbUrl, err := postgresContainer.ConnectionString(ctx, "sslmode=disable")
-	require.NoError(t, err)
+	dbUrl := pgContainer.ConnectionString
+
 	pool, err := pgxpool.New(ctx, dbUrl)
 	require.NoError(t, err)
 	db := stdlib.OpenDBFromPool(pool)
---
+++ internal/testhelpers/containers.go
@@ -0,0 +1,43 @@
+package testhelpers
+
+import (
+	"context"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"time"
+)
+
+type PostgresContainer struct {
+	*postgres.PostgresContainer
+	ConnectionString string
+}
+
+func CreatePostgresContainer(ctx context.Context) (*PostgresContainer, error) {
+	dbName := "music_digest_bot"
+	dbUser := "postgres"
+	dbPassword := "postgres"
+
+	pgContainer, err := postgres.Run(ctx,
+		"docker.io/postgres:16-alpine",
+		postgres.WithDatabase(dbName),
+		postgres.WithUsername(dbUser),
+		postgres.WithPassword(dbPassword),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresContainer{
+		PostgresContainer: pgContainer,
+		ConnectionString:  connStr,
+	}, nil
+}
